6ddab14f14b54ee006e006570fe6e34c5822c31b
mzccddkk <mzccddkk@gmail.com>
2024-03-06 18:40:11 +0800

    storage file

--- internal/connection.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-type Connection struct {
-	Name     string `json:"name" yaml:"name"`
-	Host     string `json:"host" yaml:"host"`
-	Port     int    `json:"port" yaml:"port"`
-	Username string `json:"username" yaml:"username"`
-	Password string `json:"password" yaml:"password"`
-}
--- internal/storage.go
+++ internal/storage.go
@@ -1,1 +1,28 @@
 package internal
+
+import (
+	"os"
+	"path"
+
+	"github.com/mitchellh/go-homedir"
+)
+
+type storage struct {
+	path string
+}
+
+func newStorage(filename string) *storage {
+	dir, _ := homedir.Dir()
+
+	return &storage{
+		path: path.Join(dir, filename),
+	}
+}
+
+func (s *storage) get() ([]byte, error) {
+	return os.ReadFile(s.path)
+}
+
+func (s *storage) put(data []byte) error {
+	return os.WriteFile(s.path, data, 0777)
+}
